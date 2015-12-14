96548f687279cf1e732962490a16404d84691d04
Shannon Clark <theshannonclark@gmail.com>
2015-12-14 13:36:47 +1100

    Create tournament database
    Added SQL to drop the tournament database if it exists, then create it
    again. Then the script connects to the new database.
    
    Also added some helpful comments.

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -1,9 +1,23 @@
+-- Set up tournament database for project 2
+--
+-- To run this file, connect to psql from the command
+-- line, and enter \i tournament.sql to import it.
+--
+-- More detailed instructions can be found in README.md.
+
+
+
+-- Create tournament database, and connect to it
+--
+-- Drop the database if it already exists first.
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
+
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
 
