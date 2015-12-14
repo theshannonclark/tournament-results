034251c88cb15d0ee345f7e705ba5e2919a14530
Shannon Clark <theshannonclark@gmail.com>
2015-12-14 15:04:04 +1100

    Create players and matches tables
    Added SQL to create a players table, and a matches table. The players
    table has an autoincrementing id, a first name, and a last name. The
    matches table has the ids of the two players in the match, and the
    id of the winner. The combination of the two players' ids is used as
    the primary key in matches.

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -15,9 +15,29 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 
--- Table definitions for the tournament project.
+
+
+-- Table definitions for the tournament project
 --
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
+-- Two tables: players and matches. No need to
+-- drop them first, since dropping the database
+-- already does that.
 
 
+
+-- Create the players table
+
+CREATE TABLE players (
+  player_id smallserial PRIMARY KEY,
+  first_name text,
+  last_name text
+);
+
+-- Create the matches table
+
+CREATE TABLE matches (
+  p1_id smallint REFERENCES players,
+  p2_id smallint REFERENCES players,
+  winner_id smallint REFERENCES players,
+  PRIMARY KEY (p1_id, p2_id)
+);
